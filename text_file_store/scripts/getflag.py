e71cb6c50c17592c5a5e47ac52a2da1733c5489f
Riley Childs <riley@tfsgeo.com>
2015-11-30 03:13:09 +0000

    update getflag.py

--- text_file_store/scripts/getflag.py
+++ text_file_store/scripts/getflag.py
@@ -1,32 +1,33 @@
-import socket
 import sys
 import re
 import requests
 import urllib2
 
-class WrongUsernameOrPassword(Exception)
+
+class WrongUsernameOrPassword(Exception):
     pass
 
 def get_flag(ip, port, flag_id, token):
     username = flag_id
     password = token
 
-    if ip:
-        conn = socket.create_connection(ip, port)
-        value = flag_id + '_' + token
-        url = 'http://' + ip + ':' + str(port) + '/viewFile.php'
-        r = requests.post(url, data = {'file': value})
+    
+    #conn = socket.create_connection(ip, port)
+   
+    
+    r = requests.post('http://' + ip + ':' + str(port) + '/viewFile.php' + '?userName=' + flag_id + '&userPassword=' + token , data = {
+        })
+        
+
 
     # make sure the request was successful
     assert r.status_code == 200
-
-    if (re.search('Invalid Username or Password')
+    
+    if re.search(r'Invalid Username or Password', r.text):
         raise WrongUsernameOrPassword
     flag = urllib2.urlopen(r.url).read();
 
-    if ip: conn.close()
+    
 
     return { 'FLAG': flag }
 
-if __name__ == "__main__":
-    print get_flag(None, None, sys.argv[1], sys.argv[2])
